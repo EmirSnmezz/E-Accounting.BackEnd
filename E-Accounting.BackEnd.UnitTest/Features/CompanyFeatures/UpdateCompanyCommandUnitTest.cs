8b11880f268cf7df85a0adc3faed8a1969306e82
EmirSnmezz <emircan_snmez@outlook.com>
2024-09-09 17:07:16 +0300

    UpdateCompany Operation Unit Test Wrote

---
+++ E-Accounting.BackEnd.UnitTest/Features/CompanyFeatures/UpdateCompanyCommandUnitTest.cs
@@ -0,0 +1,46 @@
+﻿using E_Accounting.Application.Features.MasterFeatures.CompanyFeatures.Commands.UpdateCompany;
+using E_Accounting.Application.Services.MasterService;
+using Moq;
+using Shouldly;
+
+namespace E_Accounting.BackEnd.UnitTest.Features.CompanyFeatures;
+public sealed class UpdateCompanyCommandUnitTest
+{
+    private readonly Mock<ICompanyService> _companyService;
+
+    public UpdateCompanyCommandUnitTest()
+    {
+        _companyService = new();
+    }
+
+    [Fact]
+    public async Task CompanyShouldNotBeNull()
+    {
+        _companyService.Setup(x => x
+                            .GetById(It.IsAny<string>()))
+                            .ReturnsAsync(new Domain.Entities.App_Entites.Company());
+    }
+
+    public async Task UpdateCompanyShouldNotBeNull()
+    {
+        UpdateCompanyCommand request = new(
+            Id: "TestId",
+            Name: "TestName",
+            DatabaseName : "TestDbName",
+            ServerName: "TestServerName",
+            UserId : "TestUserId",
+            Password : "TestPassword"
+            );
+
+        UpdateCompanyCommandHandler handler = new(_companyService.Object);
+
+        _companyService.Setup(x => x
+                            .GetById(It.IsAny<string>()))
+                            .ReturnsAsync(new Domain.Entities.App_Entites.Company());
+
+        UpdateCompanyCommandResponse response = await handler.Handle(request, default);
+        response.ShouldNotBeNull();
+        response.Message.ShouldNotBeEmpty();
+    }
+
+}
--- E-Accounting.BackEnd.UnitTest/obj/Debug/net8.0/E-Accounting.BackEnd.UnitTest.AssemblyInfo.cs
+++ E-Accounting.BackEnd.UnitTest/obj/Debug/net8.0/E-Accounting.BackEnd.UnitTest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("E-Accounting.BackEnd.UnitTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d800d01e103ccca8b4dd5a359079bf1b63b76fc3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9817fda6703f1e40a38b0ac467fced4fc5adfc04")]
 [assembly: System.Reflection.AssemblyProductAttribute("E-Accounting.BackEnd.UnitTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("E-Accounting.BackEnd.UnitTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
