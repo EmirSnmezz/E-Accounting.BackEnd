073f2315725a2e907ee277183711e4254c3d9ad6
EmirSnmezz <emircan_snmez@outlook.com>
2024-09-10 14:25:17 +0300

    CreateMainRoleAndRoleRelationShip Unit Test written

---
+++ E-Accounting.BackEnd.UnitTest/Features/AppFeature/AppFeatureUsers/MainRoleAndRoleRelationShipFeature/CreateMainRoleAndRoleRelationShipCommandUnitTest.cs
@@ -0,0 +1,43 @@
+﻿using E_Accounting.Application.Features.MasterFeatures.MainRoleAndRoleRelationShipFeatures.Commands;
+using E_Accounting.Application.Services.MainRoleAndRoleRelationShipService;
+using E_Accounting.Domain.Entities.App_Entites;
+using E_Accounting.Persistance.Service.MasterDbServices.MainRoleAndRelationShipService;
+using Moq;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Accounting.BackEnd.UnitTest.Features.AppFeature.AppFeatureUsers.MainRoleAndRoleRelationShipFeature
+{
+    public class CreateMainRoleAndRoleRelationShipCommandUnitTest
+    {
+        private readonly Mock<IMainRoleAndRoleRelationShipService> _service;
+
+        public CreateMainRoleAndRoleRelationShipCommandUnitTest()
+        {
+            _service = new();
+        }
+
+        [Fact]
+        public async Task MainRoleAndRoleRelationShipShouldBeNull()
+        {
+            MainRoleAndRoleRelationship entity = await _service.Object.GetByRoleIdAndMainRoleId(roleId : "TestId" , mainRoleId : "TestId", default);    
+            entity.ShouldBeNull();
+        }
+
+        [Fact]
+        public async Task CreateMainRoleAndRoleRelationShipCommandShouldNotBeNull()
+        {
+            CreateMainRoleAndRoleRelationShipCommand request = new(RoleId : Guid.NewGuid().ToString(), MainRoleId : Guid.NewGuid().ToString());
+            CreateMainRoleAndRoleRelationShipCommandHandler handler = new(_service.Object);
+            CreateMainRoleAndRoleRelationShipsCommandResponse response = await handler.Handle(request, default);
+            response.ShouldNotBeNull();
+            response.Message.ShouldNotBeEmpty();
+
+        }
+
+    }
+}
--- E-Accounting.BackEnd.UnitTest/obj/Release/net8.0/E-Accounting.BackEnd.UnitTest.AssemblyInfo.cs
+++ E-Accounting.BackEnd.UnitTest/obj/Release/net8.0/E-Accounting.BackEnd.UnitTest.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("E-Accounting.BackEnd.UnitTest")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d573d2f030d62d40a743dbf1ed07402e16834b7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("E-Accounting.BackEnd.UnitTest")]
